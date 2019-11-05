8fdf28be86424645236b1a1766d8f8ae915d7d32
arcsoftech <arihant_chhajed@yahoo.in>
2019-11-05 13:01:47 -0600

    created corpus reader class

--- STS/core.py
+++ STS/core.py
@@ -1,4 +1,7 @@
-from reader import Reader
+from reader  import CorpusReader
 if __name__ == "__main__":
-    reader = Reader("data")
-    print(reader.get())
+    reader = CorpusReader("data")
+    dev_set = reader.get()
+    train_set = reader.get(dev=1)
+    test_set = reader.get(dev=2)
+    print(dev_set.head(),train_set.head())
--- STS/reader.py
+++ STS/reader.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-class Reader:
+class CorpusReader:
     def  __init__(self, path):
-        self.dev  = pd.read_csv(path+"/dev-set.txt", sep='\t', lineterminator='\n')
-        self.test = pd.read_csv(path+"/train-set.txt", sep='\t', lineterminator='\r')
+        self.dev  = pd.read_csv(path+"/dev-set.txt", sep='\t',quoting=3)
+        self.train = pd.read_csv(path+"/train-set.txt", sep='\t',quoting=3)
+        self.test = pd.read_csv(path+"/test-set.txt", sep='\t',quoting=3) 
 
-    def get (self,dev=1)-> None:
-        if(dev):
+    def get (self,dev=0)-> None:
+        if dev == 0:
             return self.dev
+        elif dev == 1:
+            return self.train
         else:
             return self.test
