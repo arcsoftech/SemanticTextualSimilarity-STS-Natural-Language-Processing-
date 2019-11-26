2f8afd915843056b2fcbd3f0e0a5ce183086900e
arcsoftech <arihant_Chhajed@yahoo.in>
2019-11-26 11:00:19 -0600

    Fixed wsd serialization

--- STS/lib/Preprocessing/featureGenerator.py
+++ STS/lib/Preprocessing/featureGenerator.py
@@ -5,7 +5,6 @@ import nltk
 import pandas as pd
 from nltk.corpus import wordnet
 import spacy
-import networkx as nx
 from nltk import Tree
 import os
 from nltk.wsd import lesk
@@ -13,19 +12,14 @@ from collections import defaultdict
 from spacy.lang.en.stop_words import STOP_WORDS as eng_stopwords
 import string
 sp = spacy.load('en_core_web_sm')
-class Lemmas:
-    def __init__(self,word,tag):
-        self.text=word
-        self.tag_=tag
-    def __repr__(self):
-        return "{}|{}".format(self.text,self.tag_)
+
 def get_synsets(word):
         word_synset = wordnet.synsets(word)
         return word_synset
 
 def get_wordnet_pos(treebank_tag):
-    if treebank_tag is None:
-        return wordnet.NOUN
+    # if treebank_tag is None:
+    #     return wordnet.NOUN
     if treebank_tag.startswith('J'):
         return wordnet.ADJ
     elif treebank_tag.startswith('V'):
@@ -36,39 +30,38 @@ def get_wordnet_pos(treebank_tag):
         return wordnet.ADV
     else:
         return None
-def get_hypernyms(word):
-    word_synsets = get_synsets(word)
-    for synset in word_synsets:
-        for s in synset.hypernyms():
-            for t in s.lemmas():
-                yield t.name()
+def get_hypernyms(synset):
+    word_synset = wordnet.synset(synset)
+    for s in word_synset.hypernyms():
+        for t in s.lemmas():
+            yield t.name()
 
-def get_hyponyms(word):
-    word_synsets = get_synsets(word)
-    for synset in word_synsets:
-        for s in synset.hyponyms():
-            for t in s.lemmas():
-                yield t.name()
+def get_hyponyms(synset):
+    word_synset = wordnet.synset(synset)
+    for s in word_synset.hyponyms():
+        for t in s.lemmas():
+            yield t.name()
 
-def get_meronyms(word):
-    word_synsets = get_synsets(word)
-    for synset in word_synsets:
-        for s in synset.part_meronyms():
-            for t in s.lemmas():
-                yield t.name()
-        for s in synset.substance_meronyms():
-            for t in s.lemmas():
-                yield t.name()
 
-def get_holonyms(word):
-    word_synsets = get_synsets(word)
-    for synset in word_synsets:
-        for s in synset.part_holonyms():
-            for t in s.lemmas():
-                yield t.name()
-        for s in synset.substance_holonyms():
-            for t in s.lemmas():
-                yield t.name()
+def get_meronyms(synset):
+    word_synset = wordnet.synset(synset)
+    for s in word_synset.part_meronyms():
+        for t in s.lemmas():
+            yield t.name()
+    for s in word_synset.substance_meronyms():
+        for t in s.lemmas():
+            yield t.name()
+
+
+def get_holonyms(synset):
+    word_synset = wordnet.synset(synset)
+    for s in word_synset.part_holonyms():
+        for t in s.lemmas():
+            yield t.name()
+    for s in word_synset.substance_holonyms():
+        for t in s.lemmas():
+            yield t.name()
+
 
 def tok_format(tok):
     return "_".join([tok.orth_, tok.tag_, tok.dep_])
@@ -78,7 +71,38 @@ def to_nltk_tree(node):
         return Tree(tok_format(node), [to_nltk_tree(child) for child in node.children])
     else:
         return tok_format(node)
+class WSD:
+    def __init__(self):
 
+        self.n=set()
+        self.a=set()
+        self.v=set()
+        self.r=set()
+
+    def append(self,tag,wsd):
+        if tag is "n":
+            self.n.add(wsd)
+        if tag is "a":
+            self.a.add(wsd)
+        if tag is "v":
+            self.v.add(wsd)
+        if tag is "r":
+            self.r.add(wsd)
+    def get_synsets(self):
+        a= self.n.union(self.a).union(self.v).union(self.r)
+        a= [(x,y) for x,y in a if y is not None]
+        return a
+
+
+    def __repr__(self):
+        return str({"n":self.n,"a":self.a,"v":self.v,"r":self.r})
+
+class Lemmas:
+    def __init__(self,word,tag):
+        self.text=word
+        self.tag_=tag
+    def __repr__(self):
+        return "{}|{}".format(self.text,self.tag_)
 class Preprocessing:
     def __init__(self, data):
         self.data = data
@@ -151,42 +175,52 @@ class Preprocessing:
         """
         Generate hypernyms for tokens
         """
-        hypernyms = defaultdict(list)
-        lemmas = row['lemmas']
-        for sent in lemmas:
-            for word in sent:
-                hypernyms[word.text]=list(get_hypernyms(word.text))
-        return hypernyms
+        output=[]
+        wsd = row['lesk_wsd']
+        for sent in wsd:
+            hypernyms = defaultdict(list)
+            for word,synset in sent.get_synsets():
+                hypernyms[word]=list(get_hypernyms(synset))
+            output.append(hypernyms)
+        return output
+
     def __hyponyms__(self,row):
         """
         Generate hyponyms for tokens
         """
-        lemmas = row['lemmas']
-        hyponyms = defaultdict(list)
-        for sent in lemmas:
-            for word in sent:
-                hyponyms[word.text]=list(get_hyponyms(word.text))
-        return hyponyms
+        output=[]
+        wsd = row['lesk_wsd']
+        for sent in wsd:
+            hyponyms = defaultdict(list)
+            for word,synset in sent.get_synsets():
+                hyponyms[word]=list(get_hyponyms(synset))
+            output.append(hyponyms)
+        return output
+
     def __meronyms__(self,row):
         """
         Generate meronyms for tokens
         """
-        lemmas = row['lemmas']
-        meronyms = defaultdict(list)
-        for sent in lemmas:
-            for word in sent:
-                meronyms[word.text]=list(get_meronyms(word.text))
-        return meronyms
+        output=[]
+        wsd = row['lesk_wsd']
+        for sent in wsd:
+            meronyms = defaultdict(list)
+            for word,synset in sent.get_synsets():
+                meronyms[word]=list(get_meronyms(synset))
+            output.append(meronyms)
+        return output
     def __holonyms__(self,row):
         """
         Generate holonyms for tokens
         """
-        lemmas = row['lemmas']
-        holonyms = defaultdict(list)
-        for sent in lemmas:
-            for word in sent:
-                holonyms[word.text]=list(get_holonyms(word.text))
-        return holonyms
+        output=[]
+        wsd = row['lesk_wsd']
+        for sent in wsd:
+            holonyms = defaultdict(list)
+            for word,synset in sent.get_synsets():
+                holonyms[word]=list(get_holonyms(synset))
+            output.append(holonyms)
+        return output
     def __generateParseTree__(self,row):
         """
         Generate parsetree for sentences
@@ -201,16 +235,20 @@ class Preprocessing:
         """
         Word sense disambugation 
         """
-        lemmas = row["tokens"]
-        nv_dict= defaultdict(lambda: defaultdict(lambda: []))
-        # nv_dict = {0:{"n":[],"v":[]},1:{"n":[],"v":[]}}
-        for i,x in enumerate(lemmas):
+        tokens = row["tokens"]
+        output= []
+        for x in tokens:
+            a = WSD()
             for y in x:
                 tag = get_wordnet_pos(y.tag_)
-                if(tag in ["n","v"]):
+                if(tag in ["n","v","a","r"]):
                     wsd = lesk(x,y.text,tag)
-                    nv_dict[i][tag].append((y.text,wsd))
-        return nv_dict
+                    if wsd is not None:
+                        wsd=wsd.name()
+                    a.append(tag,(y.text,wsd))
+            output.append(a)
+            
+        return output
 
     def __get_vocab_from_lemmas_set(self,row):
         lemmas = row["lemmas"]
@@ -229,22 +267,22 @@ class Preprocessing:
         self.data["pos_tagged"] = self.data.apply(self.__pos_spacy__,axis=1)
         self.data["pos_tagged_filtered"] = self.data.apply(self.__pos_spacy_filter__,axis=1)
         self.data["lemmas"] = self.data.apply(self.__lemmatize__,axis=1)
+        self.data["lesk_wsd"] = self.data.apply(self.__wordnet_lesk_wsd__,axis=1)
         self.data["hypernyms"] = self.data.apply(self.__hypernyms__,axis=1)
         self.data["hyponyms"] = self.data.apply(self.__hyponyms__,axis=1)
         self.data["holonyms"] = self.data.apply(self.__holonyms__,axis=1)
         self.data["meronyms"] = self.data.apply(self.__meronyms__,axis=1)
         self.data['dependency_tree'] = self.data.apply(self.__generateParseTree__,axis=1)
         self.data['vocabulary'] = self.data.apply(self.__get_vocab_from_lemmas_set,axis=1)
-        self.data["lesk_wsd"] = self.data.apply(self.__wordnet_lesk_wsd__,axis=1)
+        
         return self
         
 
-    def store(self,name):
+    def store(self,file_path):
         """
         Store preprocessed data for reuse
         """
-        # print(self.data.loc[0])
-        file_path = "../PreProcessesData/{}".format(name)
+        print(self.data.loc[0])
         directory = os.path.dirname(file_path)
         try:
             os.stat(directory)
--- STS/preprocess.py
+++ STS/preprocess.py
@@ -1,11 +1,15 @@
 from lib.Preprocessing.reader import CorpusReader
 from lib.Preprocessing.featureGenerator import Preprocessing
+import os
 
 if __name__ == "__main__":
-    reader = CorpusReader("data")
+    dirname = os.path.dirname(__file__)
+    directory = os.path.join(dirname, 'data/')
+    Storedirectory = os.path.join(dirname, 'PreProcessesData/')
+    reader = CorpusReader(directory)
     dev_set = Preprocessing(reader.get())
     train_set = Preprocessing(reader.get(dev=1))
     test_set = Preprocessing(reader.get(dev=2))
-    dev_set.transform().store("dev")
-    train_set.transform().store("train")
-    test_set.transform().store("test")
+    dev_set.transform().store(Storedirectory+"dev")
+    train_set.transform().store(Storedirectory+"train")
+    test_set.transform().store(Storedirectory+"test")
